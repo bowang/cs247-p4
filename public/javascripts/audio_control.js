9cc65305f15f0f3c81f4626e09f7eef90b1a5b3d
Borui Wang <borui@stanford.edu>
2013-02-13 15:45:34 -0800

    fixed sound bug

--- public/javascripts/audio_control.js
+++ public/javascripts/audio_control.js
@@ -1,7 +1,9 @@
 var ad_context; // this is the audio context
 var buffer_loader; // object contaning all buffer of sounds
-var buffer_list_playable;
-var sound_interval_timeout;
+var local_buffer_player;
+var local_buffer_list_playable;
+var local_sound_interval_timeout;
+// sound source has all the sounds for the app
 var sound_source = [
       'sound/bziaou_1.ogg',
       'sound/bziaou_2.ogg',
@@ -18,13 +20,32 @@ var sound_source = [
       'sound/bziaou_13.ogg',
       'sound/bziaou_14.ogg',
       'sound/bziaou_15.ogg',
-      'sound/bziaou_16.ogg'
+      'sound/bziaou_16.ogg',
+      'sound/syntklocka_stab_1.ogg',
+      'sound/syntklocka_stab_2.ogg',
+      'sound/syntklocka_stab_3.ogg',
+      'sound/syntklocka_stab_4.ogg',
+      'sound/syntklocka_stab_5.ogg',
+      'sound/syntklocka_stab_6.ogg',
+      'sound/syntklocka_stab_7.ogg',
+      'sound/syntklocka_stab_8.ogg',
+      'sound/syntklocka_stab_9.ogg',
+      'sound/syntklocka_stab_10.ogg',
+      'sound/syntklocka_stab_11.ogg',
+      'sound/syntklocka_stab_12.ogg',
+      'sound/syntklocka_stab_13.ogg',
+      'sound/syntklocka_stab_14.ogg',
+      'sound/syntklocka_stab_15.ogg',
+      'sound/syntklocka_stab_16.ogg',
     ];
 var document_height;
 var mouse_doc_x, mouse_doc_y;
-var buffer_player;
 var socket;
 var my_id = Math.random().toString(36).substring(7);
+
+// for other players connected to the game
+var other_player_sound_interval_timeout = {};
+var other_player_mouse_coord = {};
 
 function create_audio_context(){
   try {
@@ -42,31 +63,33 @@ function create_audio_context(){
 // trigger then buffer of all audio clips are loaded completely
 function buffer_loading_finished(bufferList) {
   console.log("Buffer loader compplete - loaded " + sound_source.length + " sounds");
-  buffer_list_playable = bufferList;
+  local_buffer_list_playable = bufferList;
   $("#loading").fadeOut();
   initialize_socket();
   attach_mouse_events();
 }
 
 // play a particular sound clip
-function play(index){
-  if(typeof buffer_player !== "undefined"){
-    buffer_player.stop(0);
+function play(playlist,index){
+  if(typeof local_buffer_player !== "undefined"){
+    local_buffer_player.stop(0);
   }
-  buffer_player = ad_context.createBufferSource();
-  buffer_player.buffer = buffer_list_playable[index];
-  buffer_player.connect(ad_context.destination);
-  buffer_player.start(0);
+  local_buffer_player = ad_context.createBufferSource();
+  local_buffer_player.buffer = playlist[index];
+  local_buffer_player.connect(ad_context.destination);
+  local_buffer_player.start(0);
 }
 
 // play the music stream
-function play_stream(){
-  sound_interval_timeout = setInterval(function(){play(Math.floor(16*mouse_doc_y/document_height));},170);
+function local_player_play_stream(){
+  // first play once then setup interval to avoid delay
+  play(local_buffer_list_playable,Math.floor(16*mouse_doc_y/document_height));
+  local_sound_interval_timeout = setInterval(function(){play(local_buffer_list_playable,Math.floor(16*mouse_doc_y/document_height));},170);
 }
 
 // clear music
 function clear_sound_time_out(){
-  window.clearInterval(sound_interval_timeout);
+  window.clearInterval(local_sound_interval_timeout);
 }
 
 // attach mouse events to the dom
@@ -81,7 +104,7 @@ function attach_mouse_events(){
   });
   $(document).mousedown(function(e){
     console.log("mouse down");
-    play_stream();
+    local_player_play_stream();
   });
   $(document).mouseup(function(e){
     console.log("mouse up");
@@ -96,10 +119,23 @@ function initialize_socket(){
     if($("#"+data.id).length == 0){
       $("body").append("<div id='"+data.id+"' class='other_circle'></div>")
     }
+    if(typeof other_player_mouse_coord[data.id] === "undefined"){
+      other_player_mouse_coord[data.id] = {x:0,y:0};
+    }
+    other_player_mouse_coord[data.id].x = data.x;
+    other_player_mouse_coord[data.id].y = data.y;
     $("#"+data.id).css({top:data.y,left:data.x});
   });
   socket.on('other-disconnect', function (data) {
+    // data is the id here
     $("#"+data).fadeOut();
+    delete other_player_mouse_coord[data];
+  });
+  socket.on('other-mousedown', function (data) {
+    other_player_play_stream();
+  });
+  socket.on('other-mouseup', function (data) {
+    other_player_clear_sound_time_out();
   });
 }
 
